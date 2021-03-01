e3b0b5a54ed282c4538a8ae6019cd2107b5d5298
Thiago Araujo Mustafa Dos Santos <thiago.santos@geap.com.br>
2021-03-01 08:19:19 -0300

    Added class and object definitions

---
+++ OOP/01 - Class x Object/House.cs
@@ -0,0 +1,27 @@
+﻿
+namespace OOP
+{
+    //A class is da data structure that aims to represent something that exists in the real world
+    public class House
+    {
+        public int SquareFeet { get; set; }
+        public int Floors { get; set; }
+        public decimal Amount  { get; set; }
+        public int CarSpots { get; set; }
+    }
+
+    // An object is an instance of a class
+    public class Object
+    {
+        public Object()
+        {
+            var house = new House
+            {
+                SquareFeet = 100,
+                Floors = 2,
+                Amount = 100000,
+                CarSpots = 3
+            };
+        }
+    }
+}
